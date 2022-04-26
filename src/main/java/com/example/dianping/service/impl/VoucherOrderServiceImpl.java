f7608cb1ad25438e3a53a4d3897849b01e9e7a11
yuchen <yuchen.be@gmail.com>
2022-04-26 22:09:25 +0800

    优化优惠券下单逻辑，防止超售和一人多单的问题

--- src/main/java/com/example/dianping/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/example/dianping/service/impl/VoucherOrderServiceImpl.java
@@ -37,7 +37,6 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
      * @return          购买结果，下单成功则返回订单号，否则提示购买失败
      */
     @Override
-    @Transactional
     public Result purchaseLimitedVoucher(Long voucherId) {
         // 查询秒杀券的库存、开放购买时间以及结束购买时间
         LimitedVoucher voucher = limitedVoucherService.getById(voucherId);
@@ -53,27 +52,49 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return Result.fail("优惠券已抢光");
         }
 
-        // 库存足够，将库存减 1
-        boolean result = limitedVoucherService.update()
-                .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId)
-                .update();
-        if (!result) {
-            log.debug("库存更新失败");
-            return Result.fail("库存不足");
+        // 查询当前用户之前有没有购买过这张优惠券，然后尝试减库存并下单
+        // 不同的线程对于 MySQL 中 "userId 用户购买 vouchId 优惠券的订单数" 这个数据是共享的，可能会出现「线程干扰」，必须使用同步 (Synchronization)
+        return createOrder(voucherId, userId);
+    }
+
+    /**
+     * 根据用户 ID 和优惠券 ID 查询单号，如果该用户没有购买过该优惠券，则减去库存并下单。
+     * @param voucherId 优惠券 ID
+     */
+    @Transactional
+    public Result createOrder(Long voucherId, Long userId) {
+        // 对相同的 userId 采取同步
+        synchronized (userId.toString().intern()) {
+            Long count = query().eq("user_id", userId)
+                    .eq("voucher_id", voucherId)
+                    .count();
+            if (count > 1) {
+                return Result.fail("该优惠券限一人购买一张，您已购买过");
+            }
+
+            // 减库存
+            boolean result = limitedVoucherService.update()
+                    .setSql("stock = stock - 1")
+                    .eq("voucher_id", voucherId)
+                    .gt("stock", 0)         // 乐观锁的思想
+                    .update();
+            if (!result) {
+                log.debug("{} 号用户尝试下单，但是库存不足", userId);
+                return Result.fail("库存不足");
+            }
+
+            // 创建订单
+            VoucherOrder order = new VoucherOrder();
+            Long uid = uidGenerator.nextId("order");        // 订单分配到的 ID
+            order.setId(uid);
+            order.setVoucherId(voucherId);
+            order.setPayTime(LocalDateTime.now());
+
+            order.setUserId(userId);          // 用户信息到当前线程关联的 UserHolder 中取
+            save(order);
+            log.debug("用户 {} 成功购买了一张 {} 号优惠券，订单号：{}", userId, voucherId, uid);
+
+            return Result.ok(uid);
         }
-
-        // 创建订单
-        VoucherOrder order = new VoucherOrder();
-        Long uid = uidGenerator.nextId("order");        // 订单分配到的 ID
-        order.setId(uid);
-        order.setVoucherId(voucherId);
-        order.setPayTime(LocalDateTime.now());
-
-        order.setUserId(userId);          // 用户信息到当前线程关联的 UserHolder 中取
-        save(order);
-        log.debug("用户 {} 成功购买了一张 {} 号优惠券，订单号：{}", userId, voucherId, uid);
-
-        return Result.ok(uid);
     }
 }
